57b2abc04b68f1f38c48b77a9b3deddb1e219390
Clark Ritchie <clark.ritchie@specialized.com>
2022-08-04 09:13:56 -0700

    absurd debugging attempt

--- admin_functions.go
+++ admin_functions.go
@@ -5,7 +5,8 @@ import (
 )
 
 func (conn *AuthenticatedConnection) LookupUserInformationByDetails(data url.Values) (map[string]interface{}, error) {
-	req, err := conn.NewRequest("GET", "/rest/users?" + data.Encode(), nil)
+	req, err := conn.NewRequest("GET", "/rest/users?"+data.Encode(), nil)
+	req.Header.Set("User-Agent", "Go_LookupUserInformationByDetails")
 	if err != nil {
 		return nil, err
 	}
@@ -25,7 +26,7 @@ func (conn *AuthenticatedConnection) LookupUserInformationByDetails(data url.Val
 	return resultData, nil
 }
 
-func (conn *AuthenticatedConnection) LookupUserInformationByEmail(email string)  (map[string]interface{}, error) {
+func (conn *AuthenticatedConnection) LookupUserInformationByEmail(email string) (map[string]interface{}, error) {
 	return conn.LookupUserInformationByDetails(url.Values{
 		"email": {email},
 	})
@@ -39,7 +40,8 @@ func (conn *AuthenticatedConnection) LookupUserInformationByIdentifier(ident str
 }
 
 func (conn *AuthenticatedConnection) GetTokenForUserIdentifier(ident string) (string, error) {
-	req, err := conn.NewRequest("GET", "/rest/users/jwt/" + conn.Connection.ApiKey + "/" + ident + "/7200", nil)
+	req, err := conn.NewRequest("GET", "/rest/users/jwt/"+conn.Connection.ApiKey+"/"+ident+"/7200", nil)
+	req.Header.Set("User-Agent", "Go_GetTokenForUserIdentifier")
 	if err != nil {
 		return "", err
 	}
