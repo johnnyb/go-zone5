e32f8c64adc942ba5a47c97cb9a5a25e7e5a9f10
Clark Ritchie <clark.ritchie@specialized.com>
2022-09-06 11:37:00 -0700

    Testing user agent add

--- authenticated_connection.go
+++ authenticated_connection.go
@@ -1,21 +1,23 @@
 package zone5
 
 import (
-	"strings"
-	"time"
-	"net/url"
+	"fmt"
 	"io"
 	"net/http"
+	"net/url"
+	"os"
+	"strings"
+	"time"
 )
 
 var RefreshTimeMargin = time.Duration(30) * time.Second
 
 type AuthenticatedConnection struct {
-	Connection *Connection
-	AuthToken string
+	Connection   *Connection
+	AuthToken    string
 	RefreshToken string
-	Expiration *time.Time
-	Details map[string]interface{}
+	Expiration   *time.Time
+	Details      map[string]interface{}
 }
 
 func (conn *AuthenticatedConnection) GetCurrentToken() string {
@@ -28,10 +30,10 @@ func (conn *AuthenticatedConnection) GetCurrentToken() string {
 
 func (conn *AuthenticatedConnection) PerformTokenRefresh() error {
 	req, err := conn.Connection.NewRequest("POST", "/rest/oauth/access_token", strings.NewReader(url.Values{
-		"username": {conn.Details["user"].(map[string]interface{})["email"].(string)},
-		"client_id": {conn.Connection.ApiKey},
+		"username":      {conn.Details["user"].(map[string]interface{})["email"].(string)},
+		"client_id":     {conn.Connection.ApiKey},
 		"client_secret": {conn.Connection.ApiKeySecret},
-		"grant_type": {"refresh_token"},
+		"grant_type":    {"refresh_token"},
 		"refresh_token": {conn.RefreshToken},
 	}.Encode()))
 	if err != nil {
@@ -53,7 +55,7 @@ func (conn *AuthenticatedConnection) PerformTokenRefresh() error {
 	}
 
 	// Expiration time, but give 30 seconds for potential errors
-	exp := time.Now().Add(time.Duration(resultData["expires_in"].(float64)) * time.Second - RefreshTimeMargin)
+	exp := time.Now().Add(time.Duration(resultData["expires_in"].(float64))*time.Second - RefreshTimeMargin)
 	tok := resultData["access_token"].(string)
 
 	conn.Expiration = &exp
@@ -61,11 +63,24 @@ func (conn *AuthenticatedConnection) PerformTokenRefresh() error {
 	return nil
 }
 
-func (conn *AuthenticatedConnection) NewRequest(method, path string, body io.Reader) (*http.Request, error){
+func (conn *AuthenticatedConnection) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
+	var version string
+	version = os.Getenv("TASKBOT_VERSION")
+	if len(version) == 0 {
+		version = "unknown"
+	}
+	var lambda string
+	lambda = os.Getenv("AWS_LAMBDA_FUNCTION_NAME")
+	if len(lambda) == 0 {
+		version = "taskbot"
+	}
+	userAgent := fmt.Sprintf("%s/%s", lambda, version)
+
 	req, err := conn.Connection.NewRequest(method, path, body)
 	if err != nil {
 		return nil, err
 	}
-	req.Header.Add("Authorization", "Bearer " + conn.GetCurrentToken())
+	req.Header.Add("Authorization", "Bearer "+conn.GetCurrentToken())
+	req.Header.Set("User-Agent", userAgent)
 	return req, nil
 }
